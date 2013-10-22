3a50e343a36f1e3a29fdb5b4a5bdb246a99e1687
Andrey V. Kluev <andrey.kluev@gmail.com>
2013-10-22 08:53:52 +0700

    First tests of scheme
    Как видно из названий, start all off - выключает, а start all on -
    включает все каналы при старте МК.

---
+++ tests/start all off/SmoothStart/SmoothStart.c
@@ -0,0 +1,26 @@
+/*
+ * SmoothStart.c
+ *
+ * Created: 21.10.2013 23:50:38
+ *  Author: AndreyKluev
+ */ 
+
+
+#include <avr/io.h>
+
+#define outMotor 0
+#define outLight 1
+#define outLed   2
+#define inButton 3
+
+int main(void)
+{
+	// Порт
+	DDRB  = 0b00000111;	// 0 - вход, 1 - выход, PB0 - выход
+	PORTB = 0b00000001;
+
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
