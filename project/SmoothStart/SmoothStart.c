d5643b69309a68892d3d90071ab2e137c41c42a2
Andrey V. Kluev <andrey.kluev@gmail.com>
2013-10-22 09:01:32 +0700

    Blank program

---
+++ project/SmoothStart/SmoothStart.c
@@ -0,0 +1,34 @@
+﻿/*
+ * SmoothStart.c
+ *
+ * Created: 22.10.2013 08:09:36
+ *  Author: AndreyKluev
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define outMotor 0
+#define outLight 1
+#define outLed   2
+#define inButton 3
+
+ISR(TIM0_OVF_vect)
+{
+}	
+
+int main(void)
+{
+	// Init port B
+	DDRB  = 0b00000111;	// 0 - вход, 1 - выход
+	PORTB = 0b00000110;
+
+	// Init PWM
+
+	// Init Timer0
+
+    while(1)
+    {
+	}		
+}
